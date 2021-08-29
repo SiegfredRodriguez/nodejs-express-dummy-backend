7bb3141562923bede772b6ee9bad795c56f301ba
J-dar Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2021-08-30 00:32:06 +0800

    Added CORS tolerance for all '*'

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const contactRouter = require('./src/routes/contact');
+const {OK} = require("http-status-codes");
 
 let app = express();
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: OK
+};
+
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors(corsOptions));
 
 app.use('/contact', contactRouter);
 
